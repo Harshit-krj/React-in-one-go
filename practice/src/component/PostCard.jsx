b7ec72b8b8ccd9a1f2957c5cab37551cb1e288ce
Harshit <Harshit942016@gmail.com>
2026-07-06 23:29:17 +0530

    the postcard updated

--- practice/src/component/PostCard.jsx
+++ practice/src/component/PostCard.jsx
@@ -11,8 +11,7 @@ function PostCard({ $id, title, featuredImage }) {
             <img
               src={appwriteService.filePreview(featuredImage)}
               alt={title}
-              className="rounded-xl"
-            ></img>
+              className="rounded-xl"/>
           </div>
           <h2 className="text-xl font-bold">{title}</h2>
         </div>
